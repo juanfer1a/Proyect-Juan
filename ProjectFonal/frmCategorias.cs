4ee70ad282e9d00b4ae9602bf1aaec6e53f06995
juanfer1a <jundi1a@gmail.com>
2021-06-16 13:13:20 -0500

    refactorizacion frmCategorias

--- ProjectFonal/frmCategorias.cs
+++ ProjectFonal/frmCategorias.cs
@@ -26,68 +26,15 @@ namespace ProjectFonal
 
         private void btnCrearCategoria_Click(object sender, EventArgs e)
         {
-            string GuardarCategoria = "INSERT INTO tblCategoria([idstrCategoria])" +
-                       "values('" + txtNombreCategoria.Text + "')";
-            cn.conectarComando(GuardarCategoria);
+            //string GuardarCategoria = "INSERT INTO tblCategoria([idstrCategoria])" +
+            //           "values('" + txtNombreCategoria.Text + "')";
+            //cn.conectarComando(GuardarCategoria);
             //var nombreCategoria = Categoria(txtNombreCategoria.Text);
             var nombreCategoria = Json.Json.Categoria(txtNombreCategoria.Text);
-            SerializarJsonCategoria(nombreCategoria);
+            Json.Json.SerializarJsonCategoria(nombreCategoria);
 
 
-        }
-
-       
-
-        //public static List<Json.Categoria> Categorias(string nombreCategoria)
-        //{
-        //    bool bandera = false;
-        //    var categorias = deserializeJsonFile();
-
-        //    if (categorias.Count == 0)
-        //    {
-        //        categorias = new List<Json.Categoria>
-        //            {
-        //                new Json.Categoria
-        //                {
-        //                    Nombre = nombreCategoria
-
-        //                }
-
-        //            };
-        //        return categorias;
-        //    }
-
-        //    else
-        //    {
-        //        for (int i = 0; i < categorias.Count; i++)
-        //        {
-        //            if (categorias[i].Nombre == nombreCategoria)
-        //            {
-        //                bandera = true;
-        //            }
-        //        }
-        //        if (bandera)
-        //        {
-        //            MessageBox.Show("La categoria ya existe");
-        //        }
-        //        else
-        //        {
-
-        //            categorias.Add
-        //                (
-        //                    new Json.Categoria
-        //                    {
-        //                        Nombre = nombreCategoria
-        //                    }
-        //                );
-        //            bandera = false;
-
-        //        }
-        //        return categorias;
-
-        //    }
-
-        //}
+        }       
 
 
         public static void SerializarJsonCategoria(List<Json.Categoria> categorias)
@@ -98,21 +45,5 @@ namespace ProjectFonal
 
         }
 
-        public static string GetProductFromFile()
-        {
-            string productFromFile;
-            using (var reader = new StreamReader(path))
-            {
-                productFromFile = reader.ReadToEnd();
-
-            }
-            return productFromFile;
-        }
-
-        public static List<Json.Categoria> deserializeJsonFile()
-        {
-            var categoriasDeserializer = JsonConvert.DeserializeObject<List<Json.Categoria>>(GetProductFromFile());
-            return categoriasDeserializer;
-        }
     }
 }
